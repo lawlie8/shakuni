03746298e32b7b47a85467a05e2bdccebb5fe3b5
lawlie8 <lawlie8.pm.me>
2024-10-29 18:46:26 +0530

    init commit

---
+++ src/test/java/org/lawlie8/shakuni/ShakuniApplicationTests.java
@@ -0,0 +1,13 @@
+package org.lawlie8.shakuni;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShakuniApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
