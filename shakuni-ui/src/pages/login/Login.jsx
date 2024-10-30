dbcfab687bacd3819027040d332cc231a7989663
lawlie8 <lawlie8.pm.me>
2024-10-30 23:58:39 +0530

    Fixed Form Login Function

--- shakuni-ui/src/pages/login/Login.jsx
+++ shakuni-ui/src/pages/login/Login.jsx
@@ -1,32 +1,49 @@
 import { Button, Checkbox, Col, Form, Input, Row } from 'antd';
-import {LockOutlined, UserOutlined} from '@ant-design/icons'
+import { LockOutlined, UserOutlined } from '@ant-design/icons'
 import './login.css';
-
-const AuthenticateUser = {
-
-}
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 
-export function Login(){
+export function Login() {
+    const navigate = useNavigate();
+
+
+    function AuthenticateUser(values) {
+        console.log(values);
+        
+        navigate("/dashboard");
+    
+    }
+    
+
     return <div className="login-main">
         <div className='login-container'>
             <div className='login-container-header'>
                 <h2>Shakuni</h2>
             </div>
             <div className='login-container-form'>
-            <Form name="normal-login"  initialValues={{ remember: true }} onFinish={AuthenticateUser}>
-                    <Form.Item style={{ paddingTop: "10px",fontSize:'15px' }} name="username" rules={[{ required: true, message: 'Email Required!' }]} >
-                    <UserOutlined className="site-form-item-icon" /><input placeholder='Email' className='login-user-input' />
+                <Form name="normal-login" initialValues={{ remember: true }} onFinish={AuthenticateUser}>
+                    <Form.Item style={{ paddingTop: "10px", fontSize: '15px' }} name="email" rules={[{ required: true, message: 'Email Required!' }]} >
+                        <div>
+                            <UserOutlined className="site-form-item-icon" />
+                            <input placeholder='Email' className='login-user-input' />
+                        </div>
                     </Form.Item>
 
                     <Form.Item name="password" rules={[{ required: true, message: 'Password Required!' }]}>
-                    <LockOutlined className="site-form-item-icon" /> <input className='login-user-input' placeholder='Password' type="password" />
+                        <div>
+                            <LockOutlined className="site-form-item-icon" />
+                            <input className='login-user-input' placeholder='Password' type="password" />
+                        </div>
                     </Form.Item>
-                    <Form.Item name="remember_me">
-                    <Checkbox className='login-form-check-box'>Remember Me</Checkbox>
+
+                    <Form.Item name="remember_me" valuePropName="checked">
+                        <Checkbox className='login-form-check-box' defaultChecked={false}>Remember Me</Checkbox>
                     </Form.Item>
+
                     <Form.Item>
-                        <button type="primary" htmlType="submit" className="login-form-button" >Log in</button>
+                        <button type="primary" className="login-form-button" >Log in</button>
                     </Form.Item>
                 </Form>
             </div>
