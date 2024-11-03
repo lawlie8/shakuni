593da622148a273153aac05d4cf0d88817f464b7
lawlie8 <lawlie8.pm.me>
2024-11-03 17:17:21 +0530

    Added DataSource Table Mapping. Changes in Avatar Name placeHolder, Added config,profile,management pages

---
+++ shakuni-ui/src/pages/management/Management.jsx
@@ -0,0 +1,7 @@
+export default function Management(params = {params}){
+
+    
+    return <div className="management-page">
+            <h3>Management Page</h3>
+    </div>
+}
---
+++ shakuni-ui/src/pages/profile/Profile.jsx
@@ -0,0 +1,8 @@
+export default function Profile(params = {params}){
+
+    
+    return <div className="profile-page-user">
+            <h3>Profile Page</h3>
+
+    </div>
+}
