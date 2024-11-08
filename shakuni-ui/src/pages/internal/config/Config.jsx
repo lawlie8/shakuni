e3a6c0d3f9159fa3221fd748acf8d17e40c3627c
lawlie8 <lawlie8.pm.me>
2024-11-08 19:42:08 +0530

    fixed imports in GlobalFooter.jsx, added Profile,Config,Management,Jobs, About pages

--- shakuni-ui/src/pages/internal/config/Config.jsx
+++ shakuni-ui/src/pages/internal/config/Config.jsx
@@ -1,7 +1,14 @@
+import { Col, Row } from "antd";
+
 export default function Config(params = {params}){
 
     
-    return <div className="config-page">
-        <h3>Config Page</h3>
-    </div>
+    return <Row className="config-page-content">
+            <Col span={6} className="config-page-selection">
+                <h3>selection</h3>
+            </Col>
+            <Col span={18} className="config-page-content">
+                <h3>content</h3>
+            </Col>
+        </Row>
 }
