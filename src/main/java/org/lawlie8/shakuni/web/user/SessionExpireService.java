23036edb85b61ea902df3f6f9b4cdb0e6e32c7a7
lawlie8 <l.lawlie8@gmail.com>
2024-12-09 00:45:01 +0530

    axios error function added

--- src/main/java/org/lawlie8/shakuni/web/user/SessionExpireService.java
+++ src/main/java/org/lawlie8/shakuni/web/user/SessionExpireService.java
@@ -1,14 +1,11 @@
 package org.lawlie8.shakuni.web.user;
 
-import org.lawlie8.shakuni.entity.User.Users;
-import org.lawlie8.shakuni.security.config.CustomUserDetails;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Lazy;
 import org.springframework.security.core.session.SessionInformation;
 import org.springframework.security.core.session.SessionRegistry;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
@@ -23,7 +20,7 @@ public class SessionExpireService {
     private final static Logger log = LoggerFactory.getLogger(SessionExpireService.class);
 
     public void expireUserSessionByUserName(String userName){
-        log.info("Session Expire Called");
+        log.info("Session Expire Called for User : {}",userName);
         for (Object principal : sessionRegistry.getAllPrincipals()) {
             if (principal instanceof UserDetails) {
                 UserDetails userDetails = (UserDetails) principal;
