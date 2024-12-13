a75916f14a88ba5939dab69854fc8f3be20bac92
lawlie8 <l.lawlie8@gmail.com>
2024-12-13 14:50:35 +0530

    Job Page all Jobs Segment added

--- shakuni-ui/src/pages/internal/jobs/Jobs.jsx
+++ shakuni-ui/src/pages/internal/jobs/Jobs.jsx
@@ -1,14 +1,15 @@
-import { Col, Menu, Row, Tooltip } from 'antd';
+import { Col, Divider, List, Menu, Row, Statistic, Tooltip } from 'antd';
 import './jobs.css';
 import { useState } from 'react';
-import { CodeFilled, DatabaseOutlined, FileTextFilled, PlusCircleFilled, UserOutlined } from '@ant-design/icons';
+import { ArrowUpOutlined, CheckCircleOutlined, CheckOutlined, CodeFilled, DatabaseOutlined, FileTextFilled, FireFilled, FireOutlined, FunctionOutlined, HourglassOutlined, MergeOutlined, PlusCircleFilled, UserOutlined } from '@ant-design/icons';
 import Editor from './editor/Editor';
 import { useHorizontalScroll } from '../../../util/scroll';
 
 export default function Jobs(params = { params }) {
 
     const [segmentItemList, setSegmentItemList] = useState([1, 2]);
-
+    const [allJobsView, setAllJobsView] = useState(true);
+    const [recentJobs, setRecentJobs] = useState([1, 2, 3, 4])
     const [menuItems, setMenuItems] = useState([{
         key: "1",
         icon: <img src='/datasource_logo/hive_logo.svg' height="50px" width="50px" />,
@@ -30,7 +31,69 @@ export default function Jobs(params = { params }) {
     }
 
     return <div className="jobs-page">
-        <Row className="jobs-page-main">
+        <Row className='jobs-all-page' justify={"space-between"} style={{ display: !allJobsView ? 'none' : 'flex' }}>
+            <Col span={4} style={{width:"30%",position:'relative' ,top: '-10px' }}>
+                <div className='jobs-all-stat-overview'>
+                    <h2>Overview</h2>
+                    <Divider />
+                    <Row justify={"space-between"}>
+                        <Col span={8}>
+                            <Statistic
+                                title="Running"
+                                value={"11"}
+                                valueStyle={{ color: 'orange' }}
+                                prefix={<FireFilled />}
+                                suffix=""
+                            />
+                        </Col>
+                        <Col span={8}>
+                            <Statistic
+                                title="Completed"
+                                value={"26"}
+                                valueStyle={{ color: '#3f8600' }}
+                                prefix={<CheckCircleOutlined />}
+                                suffix=""
+                            />
+                        </Col>
+                        <Col span={8}>
+                            <Statistic
+                                title="Total"
+                                value={"Σ 37"}
+                                valueStyle={{ color: '#000' }}
+                                suffix=""
+                            />
+                        </Col>
+
+                    </Row>
+                </div>
+                <div className='jobs-all-recent-jobs'>
+                    <h2>Recent Jobs</h2>
+                    <Divider />
+                    <List style={{ margin: '0px', padding: '0px' }}>
+                        {
+                            recentJobs?.map((item) => (
+                                <List.Item className='jobs-recent-all-jobs-item'>
+                                    {item}
+                                </List.Item>
+                            ))
+                        }
+
+                    </List>
+                </div>
+            </Col>
+            <Col span={20} style={{width:"70%",position:'relative',top: '-10px' }}>
+                <Row className='jobs-util-segment'>
+                    <Col span={24}>
+                    
+                    </Col>
+                </Row>
+                <Row className='jobs-content-segment'>
+                    <Col span={24}>
+                    </Col>
+                </Row>
+            </Col>
+        </Row>
+        <Row className="jobs-page-main" style={{ display: allJobsView ? 'none' : 'flex' }}>
             <Col span={4} className="jobs-page-selection">
                 <Menu
                     className='jobs-page-selection-menu'
