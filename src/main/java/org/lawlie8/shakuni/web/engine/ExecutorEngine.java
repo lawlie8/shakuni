6ba3eb6fd58608e527eba291b4aa5255c1cb588e
lawlie8 <l.lawlie8@gmail.com>
2024-12-16 20:51:20 +0530

    Added Executor Service

--- src/main/java/org/lawlie8/shakuni/web/engine/ExecutorEngine.java
+++ src/main/java/org/lawlie8/shakuni/web/engine/ExecutorEngine.java
@@ -1,17 +1,20 @@
 package org.lawlie8.shakuni.web.engine;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Service;
-
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 @Configuration
 public class ExecutorEngine {
 
+    private static final Logger log = LoggerFactory.getLogger(ExecutorEngine.class);
+
     @Bean
     public ExecutorService getExecutorService() {
+        log.info("Creating New Fixed ThreadPool with {} Threads",20);
         return Executors.newFixedThreadPool(20);
     }
 }
