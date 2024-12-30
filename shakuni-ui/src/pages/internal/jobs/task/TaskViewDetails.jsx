da6ab77e401250135876df1ad15b5ce91a235d49
lawlie8 <l.lawlie8@gmail.com>
2024-12-30 16:13:19 +0530

    Updated Task Details on Tasks View Page

--- shakuni-ui/src/pages/internal/jobs/task/TaskViewDetails.jsx
+++ shakuni-ui/src/pages/internal/jobs/task/TaskViewDetails.jsx
@@ -1,38 +1,41 @@
-import { Col,Row, Select } from "antd";
+import { Col, Row, Select } from "antd";
 import './task.css';
-import {useEffect, useState } from "react";
-import { SyncOutlined, UserOutlined } from "@ant-design/icons";
+import { useEffect, useState } from "react";
+import { DeleteFilled, DeleteOutlined, SyncOutlined, UserOutlined } from "@ant-design/icons";
 import { useDispatch, useSelector } from "react-redux";
 import { getAllUsers } from "../../config/user-settings/user-setting-service";
 import { setAllUsers } from "../../config/user-settings/UserSettingSlice";
 export default function TaskViewDetails({ params }) {
-    const allUserInfo = useSelector((state)=>state.userStoreSetting.allUsers);
-    const jobItem = useSelector((state)=>state.jobStore.selectedJobItem);
+    const jobItem = useSelector((state) => state.jobStore.selectedJobItem);
     const [jobDetails, setJobDetails] = useState([]);
     const dispatch = useDispatch();
     const [selectedUserOptions, setSelectedUserOptions] = useState([]);
+    const [currentTask, setCurrentTask] = useState(undefined);
 
-    useEffect(()=>{
+    const tasksList = useSelector((state) => state.jobStore.taskList)
+    const selectedTaskId = useSelector((state) => state.jobStore.selectedTaskId)
+
+    useEffect(() => {
         getAllUsers().then((response) => {
-            dispatch(setAllUsers(response.data))
-
-        }).catch((e)=>{
-
-        }).finally(()=>{
-            setSelectedUserOptions([]);
-
-            allUserInfo.map((item)=>{
-                setSelectedUserOptions(x=>[...x,
-                    {
-                    value:item.id,
-                    label:item.userName
+            setSelectedUserOptions([])
+            response.data.map((item) => {
+                setSelectedUserOptions(x => [...x,
+                {
+                    value: item.id,
+                    label: item.userName
                 }])
             })
-    
+            dispatch(setAllUsers(response.data))
         })
-    },[])
+    }, [])
 
-    useEffect(()=>{        
+    useEffect(() => {
+        setCurrentTask(tasksList.find((x) => x.id === selectedTaskId))
+        console.log(currentTask);
+
+    }, [selectedTaskId])
+
+    useEffect(() => {
         setJobDetails([
             {
                 key: "Id",
@@ -52,16 +55,16 @@ export default function TaskViewDetails({ params }) {
             }
         ])
         //Fetch Tasks here
-    },[jobItem])
+    }, [jobItem])
 
 
-    function handleJobShareUpdate(){
-        console.log("Updating Job Share Value",jobId);
+    function handleJobShareUpdate() {
+        console.log("Updating Job Share Value", jobId);
     }
 
     return <div className="task-view-details">
         <h3 style={{ textAlign: 'start' }}>Job Details</h3>
-        <Row style={{ width: '100%', textAlign: 'start',color:'black' }}>
+        <Row style={{ width: '100%', textAlign: 'start', color: 'black' }}>
             {
                 jobDetails.map((item) => (
                     <Col span={24} style={{ borderBottom: '1px solid #dfdfdf', fontWeight: "600" }}>
@@ -83,21 +86,66 @@ export default function TaskViewDetails({ params }) {
             <Row>
                 <Col span={24}>
                     {
-                        selectedUserOptions.length === 0 ? <SyncOutlined spin/>
-                        :
-                        <Select
-                        suffixIcon={<UserOutlined style={{color:'black'}} />}
-                        defaultValue={[jobItem?.createdBy]}
-                        mode="multiple"
-                        style={{ width: '100%' }}
-                        options={selectedUserOptions} />
+                        (selectedUserOptions.length === 0 && jobDetails.length === 0) ? <SyncOutlined spin />
+                            :
+                            <Select
+                                placeholder={"Select User"}
+                                suffixIcon={<UserOutlined style={{ color: 'black' }} />}
+                                defaultValue={[]}
+                                mode="multiple"
+                                style={{ width: '100%' }}
+                                options={selectedUserOptions} />
                     }
 
                 </Col>
-                <Col style={{position:'relative'}} span={6} offset={18}>
-                    <button className='task-back-button' style={{width:'100%',marginTop:'10px'}} onClick={()=>handleJobShareUpdate()}>Share</button>
+                <Col style={{ position: 'relative' }} span={6} offset={18}>
+                    <button className='task-back-button' style={{ width: '100%', marginTop: '10px' }} onClick={() => handleJobShareUpdate()}>Share</button>
                 </Col>
             </Row>
+        </div>
+
+        <div className="current-task-details" style={{overflow:'scroll',marginTop:'20px'}}>
+            <Row style={{ width: '100%', textAlign: 'start', color: 'black' }}>
+                <Col span={12}>
+                    <h3 style={{ textAlign: 'start' }}>{currentTask?.taskName}</h3>
+                </Col>
+                <Col span={2} offset={10}>
+                    <DeleteFilled style={{fontSize:'25px',top:'50%',transform:'translateY(-50%)',position:'relative',color:'red'}}/>
+                </Col>
+            </Row>
+                <Row style={{ width: '100%', textAlign: 'start', color: 'black' }}>
+                    <Col span={12} style={{ borderBottom: '1px solid #dfdfdf', fontWeight: "600" }}>
+                        {"Type"}
+                    </Col>
+                    <Col span={12} style={{ borderBottom: '1px solid #dfdfdf', fontWeight: "600" }}>
+                        {currentTask?.taskTypeEnum}
+                    </Col>
+
+                    <Col span={12} style={{ borderBottom: '1px solid #dfdfdf', fontWeight: "600" }}>
+                        {"Date"}
+                    </Col>
+                    <Col span={12} style={{ borderBottom: '1px solid #dfdfdf', fontWeight: "600" }}>
+                        {currentTask?.taskCreationDate === undefined ? "" : new Date(currentTask?.taskCreationDate).toISOString().split("T")[0]}
+                    </Col>
+                    <Col span={12} style={{ borderBottom: '1px solid #dfdfdf', fontWeight: "600" }}>
+                        {"Time"}
+                    </Col>
+                    <Col span={12} style={{ borderBottom: '1px solid #dfdfdf', fontWeight: "600" }}>
+                        {currentTask?.taskCreationDate === undefined ? "" : new Date(currentTask?.taskCreationDate).toTimeString().split(" ")[0]}
+                    </Col>
+                    <Col span={12} style={{ borderBottom: '1px solid #dfdfdf', fontWeight: "600" }}>
+                        {"File Name"}
+                    </Col>
+                    <Col span={12} style={{ borderBottom: '1px solid #dfdfdf', fontWeight: "600" }}>
+                        {currentTask?.filePath.split("\\").pop()}
+                    </Col>
+                    <Col span={12} style={{ borderBottom: '1px solid #dfdfdf', fontWeight: "600" }}>
+                        {"Created By"}
+                    </Col>
+                    <Col span={12} style={{ borderBottom: '1px solid #dfdfdf', fontWeight: "600" }}>
+                        {currentTask?.createdBy}
+                    </Col>
+                </Row>
 
         </div>
     </div>
